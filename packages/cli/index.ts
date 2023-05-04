528d6a99af6b0d0aff9c7a1d0736f9073d0027b0
youedd <eddibili.youssef@gmail.com>
2023-05-04 14:22:51 +0200

    feat(cli): setup cli package

---
+++ packages/cli/index.ts
@@ -0,0 +1,40 @@
+import { Command } from 'commander'
+import pkg from './package.json'
+import { compile } from '@react-json-templates/compiler'
+import path from 'path'
+import fs from 'fs/promises'
+
+const CWD = process.cwd()
+const cache = {}
+
+const program = new Command()
+
+program
+  .name('react-json-templates cli')
+  .description('CLI to compile rjt templates')
+  .version(pkg.version)
+
+program.command('compile')
+  .argument('<templates...>', 'templates to compile')
+  .argument('[outDir]', 'output directory', CWD)
+  .action(async (paths: string[], outDir: string) => {
+    const outputs = paths
+      .filter(item => item.endsWith('.rjt'))
+      .map(item => ({
+        filePath: path.join(outDir, item.replace(CWD, '')),
+        generated: compile({
+          cache,
+          filePath: item,
+          compilerConfig: {
+            sourceType: 'module',
+            plugins: ['typescript', 'jsx']
+          }
+        })
+      }))
+
+    await Promise.all(
+      outputs.map(async item => { await fs.writeFile(item.filePath, item.generated) })
+    )
+  })
+
+program.parse()
